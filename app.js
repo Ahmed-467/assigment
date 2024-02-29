f2c33ea86069762ec1d90d1bc8904a105d75b503
Ahmed-467 <hplaptop03421@gmail.com>
2024-02-29 21:04:01 +0500

    assigment file created

---
+++ app.js
@@ -0,0 +1,58 @@
+var num1 = 10;
+
+if (num1 > 10){
+    console.log('if chal rh hein.')
+}else{
+    console.log('else chal raha hein.')
+}
+
+
+var num1 = 11;
+if (num1 > 10){
+    console.log('if chal raha ha!')   
+}else{
+    console.log('else chal rha ha')
+}
+
+var property = +prompt('enter your property');
+if (property > 1500000){
+    console.log('bank balance mubarak ho')
+}else{
+    console.log('Abu nh man raha')
+}
+
+
+
+
+
+var percentage = +prompt('enter your percentage!');
+if(percentage > 100){
+    console.log('bhai person to dya sahi!')
+}else if(percentage < 50){
+    console.log('maalik tum kis kam ky banday nhi ho!')
+
+}else if (percentage >= 50 && percentage <= 60){
+    console.log('c grade aya ya hein maalik!.')
+}else if(percentage >= 61 && percentage <=70){
+    console.log('B grade aya ya hein maalik')
+}
+else{
+    console.log('percentage')
+}
+
+
+
+var percentage = +prompt('enter your percentage!');
+if(percentage > 200){
+    console.log('bhai person to dya sahi!')
+}else if(percentage < 40){
+    console.log('tum riksha chalo!')
+
+}else if (percentage >= 50 && percentage <= 60){
+    console.log('busss passed ho gaya !.')
+}else if(percentage >= 61 && percentage <=70){
+    console.log('B grade aya ya hai')
+}
+else{
+    console.log('percentage')
+}
