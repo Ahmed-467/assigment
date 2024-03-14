4d74607407cbec725e56d3b84de10dbab19ccdf6
Ahmed-467 <hplaptop03421@gmail.com>
2024-03-14 14:47:38 +0500

    if else pratices

--- if else java script pratices/app.js
+++ if else java script pratices/app.js
@@ -1,8 +1,21 @@
 //qno1
-var three=+prompt("Enter Number that divisible by 3");
+// var three=+prompt("Enter Number that divisible by 3");
 
-if(three % 3  ===0){
-    console.log("This Number is Correct Because It divisible by 3")
+// if(three % 3  ===0){
+//     console.log("This Number is Correct Because It divisible by 3")
+// }else{
+//     console.log("Number is not divisible by 3")
+// }
+
+
+
+
+///qno2
+//ANus
+var num = +prompt("Enter Even Or Odd Number");
+
+if(num % 2 ===0){
+    console.log("This is an Even Number ")
 }else{
-    console.log("Number is not divisible by 3")
+    console.log("This is an Odd Number  ")
 }
