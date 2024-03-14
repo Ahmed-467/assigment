6e37d52818c3b966837d4282a644cea89a0c6abb
Ahmed-467 <hplaptop03421@gmail.com>
2024-03-14 14:55:51 +0500

    if else

--- if else java script pratices/app.js
+++ if else java script pratices/app.js
@@ -12,10 +12,23 @@
 
 ///qno2
 //ANus
-var num = +prompt("Enter Even Or Odd Number");
+// var num = +prompt("Enter Even Or Odd Number");
 
-if(num % 2 ===0){
-    console.log("This is an Even Number ")
+// if(num % 2 ===0){
+//     console.log("This is an Even Number ")
+// }else{
+//     console.log("This is an Odd Number  ")
+// }
+
+
+
+
+///qno3
+///ANUSss
+var age = +prompt("Enter Your Age");
+
+if(age > 19 ){
+    console.log("this is real time")
 }else{
-    console.log("This is an Odd Number  ")
+    console.log("this is not real time")
 }
