c7667752ddfafe6f1bc6037d687b8d4551b6983a
Ahmed-467 <hplaptop03421@gmail.com>
2024-03-14 14:23:53 +0500

    array

---
+++ array/ap.js
@@ -0,0 +1,128 @@
+// var username = 'ahmed'
+//declar
+// var username = 'usman'
+//redclared
+// username ='ammar'
+//reassign
+// console.log(username);
+
+
+
+// const username ='ahmed'
+// console.log(username);
+//const ka variable na declar ho gya na reassign
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let city1 ="karachi"
+// let city2 ="lahore"
+// let city3 ="islamabad"
+
+// let cities =['karachi','hyderabad','multan','fasialbad','shahiwal','pakpattan',20 , true , ['fb area','golimar','gulshan','baraboard']];
+//nested array
+//array mai ek sai zada value aya sakti hein array ki thuri
+
+// console.log(cities[8][3]);
+// console.log(cities.length);
+// console.log(cities[3]);
+// console.log(typeof(cities));
+
+
+
+// const fruit ='apple'
+// const fruits = ['banana' , 'orange','grapes',fruit];
+// console.log(fruits);
+
+
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+  
+  //
+// const fruits = ['banana' , 'orange','grapes',];
+//   //array mai push ka method 1 add kir da tha hein
+//   fruits.push('nashpati')
+//   console.log(fruits);
+
+
+
+//   fruits.splice('where to start',kitna item deled kirna hein,'litna item rk na hein')
+// fruits.splice(1 , 2 )
+// console.log(fruits);
+
+
+// let cities1 = "karachi";
+// let cities2 = "hyderabad";
+// let cities3 = "shukar";
+// let citie4 = "mirpur";
+
+// let subject = ['hyderabad' ,'karachi','mirpur','shukar'];
+
+// document.write(subject[1])
+
+
+// let fruit =["apple","mango","orange","grapes","nashpati"];
+
+// // fruit.splice(1 ,3);
+// let position = fruit.indexOf('mango')
+
+// console.log(position);
+
+// let text = "my name is Ahmed nadeem"
+// let wordarray = text.split(' ')
+
+// let fruits =["apple","mango","orange","grapes","nashpati"];
+// let fruits2 =['annar','kinu']
+// let newfruit = fruits.concat(fruits2, fruits)
+
+
+// let textarray = fruits.join(' - ');
+
+
+
+// console.log(newfruit);
+
+let fruits = ['apple','mango','nashpati','annar','grapes']
+let fruit =['kinu watermelon']
+fruits.unshift('watermelon kinnu')
+
+
+
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
