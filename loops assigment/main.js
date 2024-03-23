61faeb5818f16631c3e107b38f83ca9712e59750
Ahmed-467 <hplaptop03421@gmail.com>
2024-03-24 04:20:36 +0500

    loops assigment

---
+++ loops assigment/main.js
@@ -0,0 +1,18 @@
+// / 1. Write a program to display the message “Hello World” 5 times
+// in your browser using for loop.
+
+
+/// answer
+let username ="hello world"
+let ul = document.querySelector('ul')
+
+for(let i = 1; i <= 5; i++){
+    ul.innerHTML += `<li>${"hello world"} ${i}</li>`
+}
+
+
+// 2. Write a program to print numeric counting from 1 to 10.
+// for (let i = 1; i <=10; i++) {
+//     const element = array[i];
+    
+// }
